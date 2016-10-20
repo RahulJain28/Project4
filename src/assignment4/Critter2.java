884145667b96de400e8035343c3971a7f99f0429
Rahul Jain <Rahul Jain>
2016-10-20 18:14:07 -0500

    add critter1, critter2

---
+++ src/assignment4/Critter2.java
@@ -0,0 +1,25 @@
+package assignment4;
+
+/**
+ * Created by Rahul Jain
+ * It will try to walk away from every fight, as well as unsuccessfully run. Then if it cant walk, it will fight
+ * It randomly decides a direction to run in.
+ * It's toString() representation is "2"
+ * It will always reproduce and its baby will be placed in a random direction 
+ */
+public class Critter2 extends Critter{
+    public void doTimeStep() {
+        int direction = getRandomInt(8);
+        run(direction);
+        Critter4 baby = new Critter4();
+        reproduce(baby, getRandomInt(8));
+    }
+    public boolean fight(String fightStatus) {
+    	walk(getRandomInt(8));
+    	run(getRandomInt(8));
+        return true;
+    }
+    public String toString() {
+        return "2";
+    }
+}
